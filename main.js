cdfa20a57e370bfb8f42fbc72c8b64a54a3d420f
Ashwin Prakash <ashp94@gmail.com>
2018-11-26 13:24:40 -0600

    Final Version

---
+++ main.js
@@ -0,0 +1,11 @@
+const sectionE1 = document.querySelector(".article__header");
+sectionE1.textContent = ("Welcome to the Ashwin blog");
+
+const sectionE2 = document.querySelectorAll(".article__header");
+sectionE2.classList = ("important");
+
+const sectionE3 = document.querySelector(".dashed");
+sectionE3.classList= ("")
+
+const sectionE4 = document.querySelector(".article__footer");
+sectionE4.classList= ("goldenrod");
