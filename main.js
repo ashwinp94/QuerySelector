f9500f32b151599ec4ab97f1415b6d2424ec8bcf
Ashwin Prakash <ashp94@gmail.com>
2018-11-27 10:22:33 -0600

    Final version

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const sectionE1 = document.querySelector(".article__header");
 sectionE1.textContent = ("Welcome to the Ashwin blog");
 
 const sectionE2 = document.querySelectorAll(".article__header");
-sectionE2.classList = ("important");
+for (let i=0; i <sectionE2.length; i++){
+sectionE2[i].classList.add("important");
+}
 
 const sectionE3 = document.querySelector(".dashed");
 sectionE3.classList= ("")
 
 const sectionE4 = document.querySelector(".article__footer");
 sectionE4.classList= ("goldenrod");
+
